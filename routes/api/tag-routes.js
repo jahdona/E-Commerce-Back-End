4809d7b748476eba45dc428cb672de38de43a40d
Jahdonah <jahdonah@MacBook-Pro.local>
2024-07-16 23:09:35 -0400

    Updating a tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -51,8 +51,20 @@ router.post('/', async (req, res) => {
 
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(
+      { tag_name:req.body.tag_name},
+      {
+        where:{id:req.params.id,}
+      }
+      );
+      res.status(200).json(tagData);
+    
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 router.delete('/:id', async (req, res) => {
