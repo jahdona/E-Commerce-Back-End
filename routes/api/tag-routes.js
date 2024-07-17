cd633ce671a9bec9cceb687f08a7e251fe822c07
Jahdonah <jahdonah@MacBook-Pro.local>
2024-07-16 22:55:32 -0400

    Deleting functionality

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,12 +34,20 @@ router.get('/:id', async (req, res) => {
 res.status(200).json(tagData);
   
  } catch (error) {
+  res.status(500).json(error);
   
  }
 });
 
 router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+    
+  } catch (error) {
+    res.status(500).json(error);
+  }
 
 });
 
@@ -47,8 +55,24 @@ router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where:{
+        id:req.params.id,
+      }
+    });
+    if(!tagData){
+      res.status(400).json({message:'No tag of that Id'});
+      return;
+    }
+    res.status(200).json(tagData);
+    
+  } catch (error) {
+    res.status(500).json(error)
+    
+  }
 });
 
 module.exports = router;
